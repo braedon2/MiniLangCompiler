e67938eba7c7e8cb9a91dbb71e591e383d72b3aa
Braedon Mcdonald <braedonmcdonald1@gmail.com>
2020-02-10 13:42:49 -0500

    implement AST

---
+++ src/tree.c
@@ -0,0 +1,165 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+EXP *makeEXP_identifier(char *identifier)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindIdentifier;
+    exp->val.identifier = identifier;
+    return exp;
+}
+
+EXP *makeEXP_intLiteral(int intLiteral)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindIntLiteral;
+    exp->val.intLiteral = intLiteral;
+    return exp;
+}
+
+EXP *makeEXP_strLiteral(char *strLiteral)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindStrLiteral;
+    exp->val.strLiteral = strLiteral;
+    return exp;
+}
+
+EXP *makeEXP_boolLiteral(int boolLiteral)
+{
+    if (boolLiteral != 0 && boolLiteral != 1)
+    {
+        printf("bool literal must have value of 0 or 1");
+        exit(1);
+    }
+
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindBoolLiteral;
+    exp->val.boolLiteral = boolLiteral;
+    return exp;
+}
+
+EXP *makeEXP_addition(EXP *lfs, EXP *rhs)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindAddition;
+    exp->val.binary.lhs = lfs;
+    exp->val.binary.rhs = rhs;
+    return exp;
+}
+
+EXP *makeEXP_subtraction(EXP *lfs, EXP *rhs)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindSubtraction;
+    exp->val.binary.lhs = lfs;
+    exp->val.binary.rhs = rhs;
+    return exp;
+}
+
+EXP *makeEXP_multiplication(EXP *lfs, EXP *rhs)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindMultiplication;
+    exp->val.binary.lhs = lfs;
+    exp->val.binary.rhs = rhs;
+    return exp;
+}
+
+EXP *makeEXP_division(EXP *lfs, EXP *rhs)
+{
+    EXP *exp = malloc(sizeof(EXP));
+    exp->kind = k_expressionKindDivision;
+    exp->val.binary.lhs = lfs;
+    exp->val.binary.rhs = rhs;
+    return exp;
+}
+
+EXP *makeEXP_uMinus(EXP *exp)
+{
+    EXP *e = malloc(sizeof(EXP));
+    e->kind = k_expressionKindUMinus;
+    e->val.unary.exp = exp;
+    return e;
+}
+
+EXP *makeEXP_uNot(EXP *exp)
+{
+    EXP *e = malloc(sizeof(EXP));
+    e->kind = k_expressionKindUNot;
+    e->val.unary.exp = exp;
+    return e;
+}
+
+IFSTMT *makeIFSTMT_ifElse(EXP *condition, STMT_LIST *true_branch, STMT_LIST *false_branch)
+{
+    IFSTMT *ifstmt = malloc(sizeof(IFSTMT));
+    ifstmt->kind = k_ifstmtKindIfElse;
+    ifstmt->val.condition = condition;
+    ifstmt->val.true_branch = true_branch;
+    ifstmt->val.false_branch.stmt_list = false_branch;
+    return ifstmt;
+}
+
+IFSTMT *makeIFSTMT_ifElseIf(EXP *condition, STMT_LIST *true_branch, IFSTMT *false_branch)
+{
+    IFSTMT *ifstmt = malloc(sizeof(IFSTMT));
+    ifstmt->kind = k_ifstmtKindIfElseIf;
+    ifstmt->val.condition = condition;
+    ifstmt->val.true_branch = true_branch;
+    ifstmt->val.false_branch.ifstmt = false_branch;
+    return ifstmt;
+}
+
+STMT *makeSTMT_declaration(char *identifier, DeclarationType type, EXP *exp)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindDeclaration;
+    stmt->val.declaration.identifier = identifier;
+    stmt->val.declaration.type = type;
+    stmt->val.declaration.exp = exp;
+    return stmt;
+}
+
+STMT *makeSTMT_read(char *readVar)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindRead;
+    stmt->val.readVar = readVar;
+    return stmt;
+}
+
+STMT *makeSTMT_print(EXP *exp)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindPrint;
+    stmt->val.printExp = exp;
+    return stmt;
+}
+
+STMT *makeSTMT_assign(char *identifier, EXP *exp)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindAssign;
+    stmt->val.assign.identifier;
+    stmt->val.assign.exp = exp;
+    return stmt;
+}
+
+STMT *makeSTMT_if(IFSTMT *ifstmt)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindIf;
+    stmt->val.ifstmt = ifstmt;
+    return stmt;
+}
+
+STMT *makeSTMT_while(EXP *condition, STMT_LIST *body)
+{
+    STMT *stmt = malloc(sizeof(STMT));
+    stmt->kind = k_stmtKindWhile;
+    stmt->val.whilestmt.condition = condition;
+    stmt->val.whilestmt.body = body;
+    return stmt;
+}
--- src/tree.h
+++ src/tree.h
@@ -1,21 +1,20 @@
 // defines the abstract syntac tree
 
+typedef struct STMT_LIST STMT_LIST;
 typedef struct EXP EXP;
-struct EXP {
-    ExpressionKind kind;
-    union {
-        char *identifier;
-        int intLiteral;
-        char *strLiteral;
-        int boolLiteral;
-        struct { EXP *lfs; EXP *rhs; } binary;
-        struct { EXP *exp; } unary;
-    } val;
+typedef struct IFSTMT IFSTMT;
+typedef struct STMT STMT;
+
+struct STMT_LIST {
+    STMT *stmt;
+    STMT_LIST *rest;
 };
 
 typedef enum {
     k_expressionKindIdentifier,
     k_expressionKindIntLiteral,
+    k_expressionKindStrLiteral,
+    k_expressionKindBoolLiteral,
     k_expressionKindAddition,
     k_expressionKindSubtraction,
     k_expressionKindMultiplication,
@@ -24,7 +23,25 @@ typedef enum {
     k_expressionKindUNot
 } ExpressionKind;
 
-typedef struct IFSTMT IFSTMT;
+
+struct EXP {
+    ExpressionKind kind;
+    union {
+        char *identifier;
+        int intLiteral;
+        char *strLiteral;
+        int boolLiteral;
+        struct { EXP *lhs; EXP *rhs; } binary;
+        struct { EXP *exp; } unary;
+    } val;
+};
+
+typedef enum {
+    k_ifstmtKindIfElse,
+    k_ifstmtKindIfElseIf
+} IfstmtKind;
+
+
 struct IFSTMT {
     IfstmtKind kind;
     struct { 
@@ -35,24 +52,6 @@ struct IFSTMT {
             IFSTMT *ifstmt;
         } false_branch; 
     } val;
-};
-
-typedef enum {
-    k_ifstmtKindIfElse,
-    k_ifstmtKindIfElseIf
-} IfstmtKind;
-
-typedef struct STMT STMT;
-struct STMT {
-    StmtKind kind;
-    union {
-        struct { char *identifier; DeclarationType type; EXP *exp; } declaration;
-        char *readVar;
-        EXP *printExp;
-        struct { char *identifier; EXP *exp; } assign;
-        IFSTMT *ifstmt;
-        struct { EXP *condition; STMT_LIST *body; } whileStmt;
-    };
 };
 
 typedef enum {
@@ -72,8 +71,37 @@ typedef enum {
     INFERRED
 } DeclarationType;
 
-typedef struct STMT_LIST STMT_LIST;
-struct STMT_LIST {
-    STMT *stmt;
-    STMT_LIST *rest;
-}
+struct STMT {
+    StmtKind kind;
+    union {
+        struct { char *identifier; DeclarationType type; EXP *exp; } declaration;
+        char *readVar;
+        EXP *printExp;
+        struct { char *identifier; EXP *exp; } assign;
+        IFSTMT *ifstmt;
+        struct { EXP *condition; STMT_LIST *body; } whilestmt;
+    } val;
+};
+
+/* constructors */
+
+EXP *makeEXP_identifier(char *identifier);
+EXP *makeEXP_intLiteral(int intLiteral);
+EXP *makeEXP_strLiteral(char *strLiteral);
+EXP *makeEXP_boolLiteral(int boolLiteral);
+EXP *makeEXP_addition(EXP *lfs, EXP *rhs);
+EXP *makeEXP_subtraction(EXP *lfs, EXP *rhs);
+EXP *makeEXP_multiplication(EXP *lfs, EXP *rhs);
+EXP *makeEXP_division(EXP *lfs, EXP *rhs);
+EXP *makeEXP_uMinus(EXP *exp);
+EXP *makeEXP_uNot(EXP *exp);
+
+IFSTMT *makeIFSTMT_ifElse(EXP *condition, STMT_LIST *true_branch, STMT_LIST *false_branch);
+IFSTMT *makeIFSTMT_ifElseIf(EXP *condition, STMT_LIST *true_branch, IFSTMT *false_branch);
+
+STMT *makeSTMT_declaration(char *identifier, DeclarationType type, EXP *exp);
+STMT *makeSTMT_read(char *readVar);
+STMT *makeSTMT_print(EXP *exp);
+STMT *makeSTMT_assign(char *identifier, EXP *exp);
+STMT *makeSTMT_if(IFSTMT *ifstmt);
+STMT *makeSTMT_while(EXP *condition, STMT_LIST *body);
